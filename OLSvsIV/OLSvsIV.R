be4702918424edebc07d013500dcccdad9d16877
Francis DiTraglia <fditra@sas.upenn.edu>
2013-11-21 18:35:59 -0500

    Started coding OLS versus IV example
    First attempt: quick and dirty. Doesn't work yet.

---
+++ OLSvsIV/OLSvsIV.R
@@ -0,0 +1,126 @@
+#Filename:        OLSvsIV.R
+#Author:          Frank DiTraglia
+#First Version:   2013-21-11
+#This Version:    2013-21-11
+
+#This script is a preliminary implementation of the OLS versus IV example that I am adding to the fmsc paper.
+
+
+
+dgp <- function(b, PI, V.e, V.z, n){
+  
+  #------------------------------------------------------------------
+  #NOTE: this function assumes that the library MASS has been loaded
+  #------------------------------------------------------------------
+  #Arguments:
+  # b       coefficient on x in the second stage
+  # PI      vector of coefficients on z in the second stage
+  # V.e     variance-covariance matrix of the errors epsilon and v
+  # V.z     variance-covariance matrix of the instruments z
+  # n       sample size
+  #
+  #Returns:
+  # data    list of matrices x, y and z containing simulated dataset
+  #------------------------------------------------------------------ 
+  
+  z <- mvrnorm(n, mu = rep(0, length(PI)), Sigma = V.z)
+  errors <- mvrnorm(n, mu = c(0, 0), Sigma = V.e)
+  e <- errors[,1]
+  v <- errors[,2]
+  
+  x <- z %*% PI + v
+  y <- b * x + e
+  
+  data <- list(x = x, y = y, z = z)
+  return(data)
+  
+}
+
+
+
+fmsc.ols.iv <- function(x, y, z){
+  
+  #------------------------------------------------------------------
+  #NOTE: This function assumes that the library sem has been loaded
+  #      and that x is a column of observations for a single 
+  #      endogenous regressor. In other words, it assumes that all
+  #      exogenous regressors, including the constant, have been
+  #      "projected out" of the system.
+  #------------------------------------------------------------------
+  #Arguments:
+  # x       matrix of observations of single endogenous regressor
+  # y       matrix of observations for outcome variable
+  # z       matrix of observations for the instruments
+  #------------------------------------------------------------------
+  
+  ols.fit <- lm(y ~ x - 1) 
+  b.ols <- ols.fit$coefficients
+
+  tsls.fit <-  tsls(y ~ x - 1, ~ z)
+  b.tsls <- tsls.fit$coefficients
+  
+  s.e.squared <- crossprod(tsls.fit$residuals) / n
+  
+  tau <- crossprod(x, y - b.tsls * x) / sqrt(n)
+  s.x.squared <- crossprod(x) / n
+  
+  #The following line is neither efficient nor robust: it simply follows the formula exactly. I'll replace it with something better soon.
+  g.squared <- crossprod(x, z) %*% solve(crossprod(z)) %*% crossprod(z, x) / n
+  s.v.squared <- s.x.squared - g.squared
+  
+  Tfmsc <- tau^2 * g.squared / (s.v.squared * s.e.squared * s.x.squared)
+  b.fmsc <- ifelse(Tfmsc < 2, b.ols, b.tsls)
+  
+  #Tdhw <- n * (b.ols - b.tsls)^2 / (s.e.squared * (1 / g.squared - 1 / s.x.squared))
+  
+  tau.squared.est <- tau^2 - s.e.squared * s.x.squared * s.v.squared / g.squared
+  
+  avar.est.tsls <- s.e.squared / g.squared
+  avar.est.ols <- s.e.squared / s.x.squared
+  abias.squared.est.ols <- tau.squared.est / s.x.squared^2
+  
+  
+  a <- max(0, abias.squared.est.ols)
+  b <- avar.est.ols - avar.est.tsls
+  omega.star <- 1 / (1 - (a / b))
+  b.star <- omega.star * b.ols + (1 - omega.star) * b.tsls
+  
+  out <- c(b.fmsc, b.star, b.ols, b.tsls, omega.star)
+  names(out) <- c('b.fmsc', 'b.star', 'b.ols', 'b.tsls', omega.star)
+  return(out)
+  
+}
+
+
+simple.sim <- function(p, r, n){
+
+  PI <- p * rep(1, 3)
+  V.z <- diag(rep(1, 3))
+  V.e <- diag(rep(1, 2)) + matrix(c(0, r, r, 0), 2, 2)
+  
+  sim.data <- dgp(1, PI, V.e, V.z, n)
+  x <- sim.data$x
+  y <- sim.data$y
+  z <- sim.data$z
+  b <- fmsc.ols.iv(x, y, z)
+  return(b)
+  
+}
+
+mse <- function(x, truth){mean((x - truth)^2)}
+
+
+mse.compare <- function(p, r, n){
+  
+  sim.results <- replicate(1000, simple.sim(p, r, n))
+  out <- apply(sim.results, 1, mse, truth = 1)
+  return(out)
+}
+
+#mse.compare(0.2, 0, 100)
+
+
+sim.results <- replicate(1000, simple.sim(0.4, 0.4, 100))
+
+
+
