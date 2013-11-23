7b8fcbfe5fceca8dc3f0ac4a475e928cf0a787a9
Francis DiTraglia <fditra@sas.upenn.edu>
2013-11-23 17:29:36 -0500

    Continued writing fmsc_ols_iv_cpp
    Still not finished. I'll implement without the DHW pretest estimators
    and averaging to start. After I get this working and see whether the
    gains in speed are worth it, I may implement a more complete version.

--- OLSvsIV/fmsc_ols_iv.cpp
+++ OLSvsIV/fmsc_ols_iv.cpp
@@ -14,7 +14,11 @@ a single endogenous regressor.
 using namespace Rcpp;
 
 // [[Rcpp::export]]
-List fmsc_ols_iv_cpp(x, y, z, DHW_levels){
+List fmsc_ols_iv_cpp(NumericVector x_r, NumericVector y_r, 
+                      NumericMatrix z_r){ 
+                        
+                      //Add DHW and averaging stuff later
+  
   /*------------------------------------------------------------------
   NOTE: This function assumes that x is a column of observations for 
         a single endogenous regressor. In other words, it assumes 
@@ -31,6 +35,36 @@ List fmsc_ols_iv_cpp(x, y, z, DHW_levels){
                      and FMSC-selected estimators, the corresponding 
                      DHW pretest estimators.
   ------------------------------------------------------------------*/
+//  zz.inv <- chol2inv(qr.R(qr(z))) 
+//  x.hat <- z %*% (zz.inv %*% zx)
+//  b.tsls <- crossprod(x.hat, y) / crossprod(x.hat)
+//  tsls.residuals <- y - x %*% b.tsls
+//  s.e.squared <- crossprod(tsls.residuals) / n
+//  tau <- crossprod(x, tsls.residuals) / sqrt(n)
+//  s.x.squared <- xx / n
+//  g.squared <- t(zx) %*% zz.inv %*% zx / n
+//  s.v.squared <- s.x.squared - g.squared
+//  Tfmsc <- tau^2 * g.squared / (s.v.squared * s.e.squared * s.x.squared)
+//  b.fmsc <- ifelse(Tfmsc < 2, b.ols, b.tsls)
+
+  int n = y_r.size();
+  int n_z = z_r.ncol();
+  
+  //Initialize armadillo matrices and vectors corresponding to 
+  //R input and reuse original memory
+  arma::colvec x(x_r.begin(), n, false);
+  arma::colvec y(y_r.begin(), n, false);
+  arma::mat z(z_r.begin(), n, n_z, false);
+  
+  //Calculations for OLS estimator
+  double xx = dot(x, x);
+  double b_ols = dot(x, y) / xx
+  
+  //Calculations for 2SLS Estimator
+  arma::colvec zx = trans(z) * x;
+  
+  
+  
   return;
   
 }
