7ef81bd6bce32c9b0a4363c40e93e4e7f0a6900b
Francis DiTraglia <fditra@sas.upenn.edu>
2013-11-25 13:46:10 -0500

    C++ versions of simple.sim and mse.compare
    Added functions simple.sim.cpp and mse.compare that call
    fmsc_ols_iv_cpp rather than the R version fmsc.ols.iv. Also added code
    for testing the different versions and benchmarking the results. The
    C++ versions give numerically identical results, as expected, but are
    significantly faster. The mclapply calculation takes about 19 seconds
    for the R version versus 11 seconds for the C++ version! The next step
    is to implement the DHW pre-test estimators in C++ and make sure they
    work correctly. After that I need to implement the corrected confidence
    intervals in C++. This will probably be somewhat involved.

--- OLSvsIV/OLSvsIV.R
+++ OLSvsIV/OLSvsIV.R
@@ -138,6 +138,14 @@ simple.sim <- function(p, r, n){
   return(b)
 }
 
+#Faster version that calls a C++ function
+simple.sim.cpp <- function(p, r, n){
+  
+  sim.data <- dgp_cpp(1, p * rep(1, 3), matrix(c(1, r, r, 1), 2, 2), diag(rep(1, 3)), n)
+  b <- fmsc_ols_iv_cpp(sim.data$x, sim.data$y, sim.data$z)
+  return(b)
+}
+
 
 mse <- function(x, truth){mean((x - truth)^2)}
 
@@ -150,12 +158,42 @@ mse.compare <- function(p, r, n, n.reps = 10000){
   return(out)
 }
 
+#Faster version that calls a C++ version
+mse.compare.cpp <- function(p, r, n, n.reps = 10000){
+  
+  sim.results <- replicate(n.reps, simple.sim.cpp(p, r, n))
+  out <- t(apply(sim.results, 1, mse, truth = 1))
+  return(out)
+}
+
+
+
+library(microbenchmark)
+system.time(mse.compare.cpp(0.3, 0.2, 250))
+system.time(mse.compare(0.3, 0.2, 250))
+
+set.seed(3728)
+barR <- mse.compare(0.3, 0.2, 250)
+set.seed(3728)
+barCpp <- mse.compare.cpp(0.3, 0.2, 250)
+all.equal(barR, barCpp)
 
 
 r.seq <- seq(0, 0.2, 0.01)
 
+system.time(do.call(rbind, mclapply(X = r.seq, FUN = function(r){mse.compare(p = 0.3, r, n = 250)}, mc.set.seed = FALSE)))
+
+system.time(do.call(rbind, mclapply(X = r.seq, FUN = function(r){mse.compare.cpp(p = 0.3, r, n = 250)}, mc.set.seed = FALSE)))
+
+
 set.seed(4938)
-fooCpp <- do.call(rbind, mclapply(X = r.seq, FUN = function(r){mse.compare(p = 0.3, r, n = 250)}, mc.set.seed = FALSE))#mclapply outputs a list of vectors. Combine them into a matrix.
+fooR <- do.call(rbind, mclapply(X = r.seq, FUN = function(r){mse.compare(p = 0.3, r, n = 250)}, mc.set.seed = FALSE))#mclapply outputs a list of vectors. Combine them into a matrix.
+
+set.seed(4938)
+fooCpp <- do.call(rbind, mclapply(X = r.seq, FUN = function(r){mse.compare.cpp(p = 0.3, r, n = 250)}, mc.set.seed = FALSE))
+
+all.equal(fooR, fooCpp)
+
 
 matplot(r.seq, apply(fooCpp, 2, sqrt))
         #, col = c('black', 'red', 'blue'), xlab = 'Cor(e,v)', ylab = 'RMSE', type =  'l', lty = 1)
