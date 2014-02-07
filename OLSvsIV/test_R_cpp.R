3fad4ef18d6f9b6421c8231beb5177cd02903f9e
Francis DiTraglia <fditra@sas.upenn.edu>
2014-02-07 10:35:03 -0500

    removed some testing code
    I've now thoroughly tested the R and C++ versions of the existing
    functions and benchmarked them. Since there are no discrepancies and I
    know that the C++ code is about 3.5 times faster, I've removed some of
    the testing code which is no longer needed.

--- OLSvsIV/test_R_cpp.R
+++ OLSvsIV/test_R_cpp.R
@@ -8,31 +8,7 @@ sourceCpp("simulation_functions_OLSvsIV.cpp")
 source("simulation_functions_OLSvsIV.R")
 
 
-set.seed(2)
-foo <- simple.sim(0.3, 0.2, 250)
-set.seed(2)
-bar <- simple_sim_cpp(0.3, 0.2, 250)
-all.equal(foo, bar)
-
-
-
-library(microbenchmark)
-#microbenchmark(mse.compare.cpp(0.3, 0.2, 250), mse.compare(0.3, 0.2, 250))
-
-set.seed(3728)
-barR <- mse.compare(0.3, 0.2, 250, 10000)
-set.seed(3728)
-barCpp <- mse_compare_default_cpp(0.3, 0.2, 250, 10000)
-  
-sum(abs(barR - barCpp))
-
-
 r.seq <- seq(0, 0.2, 0.01)
-
-system.time(do.call(rbind, mclapply(X = r.seq, FUN = function(r){mse.compare(0.3, r, 250, 10000)}, mc.set.seed = FALSE)))
-
-system.time(do.call(rbind, mclapply(X = r.seq, FUN = function(r){mse_compare_default_cpp(0.3, r, 250, 10000)}, mc.set.seed = FALSE)))
-
 
 set.seed(4938)
 fooR <- do.call(rbind, mclapply(X = r.seq, FUN = function(r){mse.compare(0.3, r, 250, 10000)}, mc.set.seed = FALSE))#mclapply outputs a list of vectors. Combine them into a matrix.
