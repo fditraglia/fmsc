fe067882470dcf44dc6747782146d17ed44520e5
Francis DiTraglia <fditra@sas.upenn.edu>
2014-02-10 15:53:11 -0500

    moved some functions around and corrected a mistake

--- OLSvsIV/simulation_functions_OLSvsIV.cpp
+++ OLSvsIV/simulation_functions_OLSvsIV.cpp
@@ -418,33 +418,6 @@ arma::rowvec fmsc_OLS_IV::CI_Lambda_fmsc(double alpha, double tau_star){
   return(out);
 }
 
-arma::rowvec fmsc_OLS_IV::CI_Lambda_AVG(double alpha, double tau_star){
-//Member function of class fmsc_OLS_IV
-//Constructs a (1 - alpha) * 100% CI for Lambda at a given value of tau 
-//based on feasible AMSE averaging. This function assumes draw_CI_sims has
-//already been called so that the data member CI_sims is available.
-  arma::rowvec one_vec = arma::ones<arma::rowvec>(CI_sims.n_cols);
-  arma::rowvec A = (tau_star / s_x_sq) * one_vec + CI_sims.row(0);
-  arma::rowvec B = CI_sims.row(1);
-  arma::rowvec C = tau_star * one_vec + CI_sims.row(2);
-  
-  arma::rowvec sq_bias_est = (pow(C, 2)  - tau_var * one_vec) 
-                                / pow(s_x_sq, 2);
-  arma::rowvec zero_vec = arma::zeros<arma::rowvec>(CI_sims.n_cols);
-  sq_bias_est = max(zero_vec, sq_bias_est);
-  
-  double var_diff = s_e_sq_tsls * (1 / g_sq - 1 / s_x_sq);
-  arma::rowvec w = one_vec / (one_vec + sq_bias_est / var_diff * one_vec);
-  arma::colvec Lambda = arma::conv_to<arma::colvec>
-                          ::from(w % A + (one_vec - w) % B);
-  double lower = sample_quantile(Lambda, alpha/2);
-  double upper = sample_quantile(Lambda, 1 - alpha/2);
-  arma::rowvec out(2);
-  out(0) = lower;
-  out(1) = upper;
-  return(out);
-}
-
 
 arma::rowvec fmsc_OLS_IV::CI_fmsc_1step(double alpha){
 //Member function of class fmsc_OLS_IV
@@ -460,24 +433,6 @@ arma::rowvec fmsc_OLS_IV::CI_fmsc_1step(double alpha){
   out(1) = upper;
   return(out);
 }
-
-
-
-arma::rowvec fmsc_OLS_IV::CI_AVG_1step(double alpha){
-//Member function of class fmsc_OLS_IV
-//Returns 1-step corrected confidence interval (lower, upper)
-//This function assumes that draw_CI_sims has already been called
-  arma::rowvec Lambda_interval = CI_Lambda_AVG(alpha, tau);
-  double Lambda_lower = Lambda_interval(0);
-  double Lambda_upper = Lambda_interval(1);
-  double lower = b_AVG() - Lambda_upper / sqrt(n);
-  double upper = b_AVG() - Lambda_lower / sqrt(n);
-  arma::rowvec out(2);
-  out(0) = lower;
-  out(1) = upper;
-  return(out);
-}
-
 
 arma::rowvec fmsc_OLS_IV::CI_fmsc_correct(double alpha, 
                                   double delta, int n_grid){
@@ -510,6 +465,49 @@ arma::rowvec fmsc_OLS_IV::CI_fmsc_correct(double alpha,
   return(out);
 }
 
+
+arma::rowvec fmsc_OLS_IV::CI_Lambda_AVG(double alpha, double tau_star){
+//Member function of class fmsc_OLS_IV
+//Constructs a (1 - alpha) * 100% CI for Lambda at a given value of tau 
+//based on feasible AMSE averaging. This function assumes draw_CI_sims has
+//already been called so that the data member CI_sims is available.
+  arma::rowvec one_vec = arma::ones<arma::rowvec>(CI_sims.n_cols);
+  arma::rowvec A = (tau_star / s_x_sq) * one_vec + CI_sims.row(0);
+  arma::rowvec B = CI_sims.row(1);
+  arma::rowvec C = tau_star * one_vec + CI_sims.row(2);
+  
+  arma::rowvec sq_bias_est = (pow(C, 2)  - tau_var * one_vec) 
+                                / pow(s_x_sq, 2);
+  arma::rowvec zero_vec = arma::zeros<arma::rowvec>(CI_sims.n_cols);
+  sq_bias_est = max(zero_vec, sq_bias_est);
+  
+  double var_diff = s_e_sq_tsls * (1 / g_sq - 1 / s_x_sq);
+  arma::rowvec w = one_vec / (one_vec + sq_bias_est / var_diff);
+  arma::colvec Lambda = arma::conv_to<arma::colvec>
+                          ::from(w % A + (one_vec - w) % B);
+                          
+  double lower = sample_quantile(Lambda, alpha/2);
+  double upper = sample_quantile(Lambda, 1 - alpha/2);
+  arma::rowvec out(2);
+  out(0) = lower;
+  out(1) = upper;
+  return(out);
+}
+
+arma::rowvec fmsc_OLS_IV::CI_AVG_1step(double alpha){
+//Member function of class fmsc_OLS_IV
+//Returns 1-step corrected confidence interval (lower, upper)
+//This function assumes that draw_CI_sims has already been called
+  arma::rowvec Lambda_interval = CI_Lambda_AVG(alpha, tau);
+  double Lambda_lower = Lambda_interval(0);
+  double Lambda_upper = Lambda_interval(1);
+  double lower = b_AVG() - Lambda_upper / sqrt(n);
+  double upper = b_AVG() - Lambda_lower / sqrt(n);
+  arma::rowvec out(2);
+  out(0) = lower;
+  out(1) = upper;
+  return(out);
+}
 
 
 arma::rowvec fmsc_OLS_IV::CI_AVG_correct(double alpha, 
@@ -639,8 +637,8 @@ arma::mat test_CIs_cpp(double p , double r, int n,
     fmsc_OLS_IV sim_results(sim_data.x, sim_data.y, sim_data.z);
     
     sim_results.draw_CI_sims(1000);
-    out.row(i) = sim_results.CI_fmsc_correct(0.03, 0.07, 100);
-    //out.row(i) = sim_results.CI_fmsc_1step(0.05);
+    //out.row(i) = sim_results.CI_fmsc_correct(0.03, 0.07, 100);
+    out.row(i) = sim_results.CI_AVG_1step(0.05);
  
   }
   return(out);  
