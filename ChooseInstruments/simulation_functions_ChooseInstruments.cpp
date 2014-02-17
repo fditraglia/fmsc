f377dfe024d2956332043f3c05069f4faf6f5def
Francis DiTraglia <fditra@sas.upenn.edu>
2014-02-17 16:00:39 -0500

    small changes
    Found error: missing semicolon at end of class declaration. Small
    additions.

--- ChooseInstruments/simulation_functions_ChooseInstruments.cpp
+++ ChooseInstruments/simulation_functions_ChooseInstruments.cpp
@@ -14,17 +14,22 @@ class LinearGMM {
     colvec b_tsls;
     mat Qz, Rz, Xtilde, Qtilde, Rtilde;
 
-}
+};
 
 
 //Class constructor
 LinearGMM::LinearGMM(const mat& X, const colvec& y, const mat& Z){
   
   qr_econ(Qz, Rz, Z);
-  Xtilde = trans(Qz) * X;
+  Xtilde = Qz.t() * X;
   qr_econ(Qtilde, Rtilde, Xtilde);
-  b_tsls = solve(Rtilde, Qtilde * trans(Qz) * y);
+  b_tsls = solve(trimatu(Rtilde), Qtilde.t() * Qz.t() * y);
   
+}
+
+
+colvec LinearGMM::tsls_est(){
+  return(b_tsls);
 }
 
 // Below is a simple example of exporting a C++ function to R. You can
@@ -34,6 +39,7 @@ LinearGMM::LinearGMM(const mat& X, const colvec& y, const mat& Z){
 // For more on using Rcpp click the Help button on the editor toolbar
 
 // [[Rcpp::export]]
-int timesTwo(int x) {
-   return x * 2;
+colvec tsls_cpp(mat X, colvec y, mat Z) {
+   LinearGMM results(X, y, Z);
+   return(results.tsls_est());
 }
