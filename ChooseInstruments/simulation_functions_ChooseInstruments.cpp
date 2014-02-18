e1f344c51ccf25290e587d3741193aad7b7c635a
Francis DiTraglia <fditra@sas.upenn.edu>
2014-02-18 18:07:36 -0500

    variance matrix estimators, various small changes
    Code to calculate the estimates still works as before but I have yet to
    test the member functions that calculate variance matrices

--- ChooseInstruments/simulation_functions_ChooseInstruments.cpp
+++ ChooseInstruments/simulation_functions_ChooseInstruments.cpp
@@ -10,59 +10,74 @@ class tsls_fit {
     colvec estimate(); //Return tsls estimate
   private:
     int n, k;
+    double s_sq;
     colvec b, residuals;
-    mat Z_copy, Qz, Rz, Xtilde, Qtilde, Rtilde, C;
-    mat Omega();
+    mat Z_copy, Qz, Rz, Xtilde, Qtilde, Rtilde, Rtilde_inv, D, C;
+    mat Omega_textbook();
     mat Omega_robust();
     mat Omega_center();
-    //mat V();
-    //mat V_robust();
-    //mat V_center();
+    mat V_textbook();
+    mat V_robust();
+    mat V_center();
+//    colvec SE_textbook();
+//    colvec SE_robust();
+//    colvec SE_center();
 };
 
 
 //Class constructor
 tsls_fit::tsls_fit(const mat& X, const colvec& y, const mat& Z){
+  n = residuals.n_elem;
+  k = Z.n_rows;
+  Z_copy = Z; 
   qr_econ(Qz, Rz, Z);
   Xtilde = Qz.t() * X;
   qr_econ(Qtilde, Rtilde, Xtilde);
   b = solve(trimatu(Rtilde), Qtilde.t() * Qz.t() * y);
   residuals = y - b * X;
-  n = residuals.n_elem;
-  k = Z.n_rows;
-  Z_copy = Z; 
-  C = solve(trimatu(Rtilde), Qtilde * 
-              solve(trimatl(Rtilde.t()), eye(k, k)));
+  D =  diagmat(pow(residuals, 2));
+  s_sq = dot(residuals, residuals) / n;
+  Rtilde_inv = solve(trimatu(Rtilde), eye(k, k));
+  C = solve(trimatu(Rtilde), Qtilde * Rtilde_inv.t());
 }
 
-mat tsls_fit::Omega(){
+mat tsls_fit::Omega_textbook(){
 //Assumes heteroskedastic errors
-  double s_sq = dot(residuals, residuals) / n;
   return(s_sq * Rz.t() * Rz / n);
 }
 
 mat tsls_fit::Omega_robust(){
 //Heteroskedasticity-robust
-  mat D = diagmat(pow(residuals, 2));
   return(Z_copy.t() * D * Z_copy / n);
 }
 
 mat tsls_fit::Omega_center(){
 //Heteroskedasticity-robust and centered
   mat e_outer = residuals * residuals.t();
-  mat D = diagmat(pow(residuals, 2));
   return(Z_copy.t() * (D / n -  e_outer / (n * n)) * Z_copy);
+}
+
+mat tsls_fit::V_textbook(){
+//Covariance matrix estimator for sqrt(n) * (b_hat - b_true)
+//Assumes heteroskedasticity
+  return(n * s_sq * Rtilde_inv * Rtilde_inv.t());
+}
+
+mat tsls_fit::V_robust(){
+//Covariance matrix estimator for sqrt(n) * (b_hat - b_true)
+//Heteroskedasticity-robust
+  return(n * n * C * Omega_robust() * C.t());
+}
+
+mat tsls_fit::V_center(){
+//Covariance matrix estimator for sqrt(n) * (b_hat - b_true)
+//Heteroskedasticity-robust and centered
+  return(n * n * C * Omega_center() * C.t());
 }
 
 colvec tsls_fit::estimate(){
   return(b);
 }
-
-// Below is a simple example of exporting a C++ function to R. You can
-// source this function into an R session using the Rcpp::sourceCpp 
-// function (or via the Source button on the editor toolbar)
-
-// For more on using Rcpp click the Help button on the editor toolbar
 
 // [[Rcpp::export]]
 colvec tsls_cpp(mat X, colvec y, mat Z) {
