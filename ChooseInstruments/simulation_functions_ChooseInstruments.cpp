be62775605838823c0c38f7f9e8f4887f86da5f9
Francis DiTraglia <fditra@sas.upenn.edu>
2014-02-18 17:19:41 -0500

    Additions to LinearGMM class

--- ChooseInstruments/simulation_functions_ChooseInstruments.cpp
+++ ChooseInstruments/simulation_functions_ChooseInstruments.cpp
@@ -11,22 +11,47 @@ class LinearGMM {
     //colvec twostep_est(); //Return efficient two-step GMM estimate
     
   private:
-    colvec b_tsls;
-    mat Qz, Rz, Xtilde, Qtilde, Rtilde;
-
+    int n, k;
+    colvec b_tsls, resid_tsls;
+    mat Z_copy, Qz, Rz, Xtilde, Qtilde, Rtilde, C;
+    mat Omega_tsls();
+    mat Omega_tsls_robust();
+    mat Omega_tsls_center();
 };
 
 
 //Class constructor
 LinearGMM::LinearGMM(const mat& X, const colvec& y, const mat& Z){
-  
   qr_econ(Qz, Rz, Z);
   Xtilde = Qz.t() * X;
   qr_econ(Qtilde, Rtilde, Xtilde);
   b_tsls = solve(trimatu(Rtilde), Qtilde.t() * Qz.t() * y);
-  
+  resid_tsls = y - b_tsls * X;
+  n = resid_tsls.n_elem;
+  k = Z.n_rows;
+  Z_copy = Z; 
+  C = solve(trimatu(Rtilde), Qtilde * 
+              solve(trimatl(Rtilde.t()), eye(k, k)));
 }
 
+mat LinearGMM::Omega_tsls(){
+//Assumes heteroskedastic errors
+  double s_sq = dot(resid_tsls, resid_tsls) / n;
+  return(s_sq * Rz.t() * Rz / n);
+}
+
+mat LinearGMM::Omega_tsls_robust(){
+//Heteroskedasticity-robust
+  mat D = diagmat(pow(resid_tsls, 2));
+  return(Z_copy.t() * D * Z_copy / n);
+}
+
+mat LinearGMM::Omega_tsls_center(){
+//Heteroskedasticity-robust and centered
+  mat e_outer = resid_tsls * resid_tsls.t();
+  mat D = diagmat(pow(resid_tsls, 2));
+  return(Z_copy.t() * (D / n -  e_outer / (n * n)) * Z_copy);
+}
 
 colvec LinearGMM::tsls_est(){
   return(b_tsls);
