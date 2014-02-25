bbc29cbd09f10506cb8ece47bb654052f63425ff
Francis DiTraglia <fditra@sas.upenn.edu>
2014-02-25 18:58:13 -0500

    simplified my minimalist R code for canonical correlations

--- ChooseInstruments/canonical_correlations_test.R
+++ ChooseInstruments/canonical_correlations_test.R
@@ -13,16 +13,18 @@ nr <- nrow(x)
 ncx <- ncol(x)
 ncy <- ncol(y)
 xcenter <- colMeans(x, )
-x <- x - rep(xcenter, rep.int(nr, ncx))
+x <- x - rep(xcenter, rep(nr, ncx))
 ycenter <- colMeans(y)
-y <- y - rep(ycenter, rep.int(nr, ncy))
+y <- y - rep(ycenter, rep(nr, ncy))
 qx <- qr(x)
 qy <- qr(y)
 dx <- qx$rank
 dy <- qy$rank
-z <- svd(qr.qty(qx, qr.qy(qy, diag(1, nr, dy)))[1L:dx,,drop = FALSE], dx, dy)
-xcoef <- backsolve((qx$qr)[1L:dx, 1L:dx, drop = FALSE], z$u)
-rownames(xcoef) <- colnames(x)[qx$pivot][1L:dx]
-ycoef <- backsolve((qy$qr)[1L:dy, 1L:dy, drop = FALSE], z$v)
-rownames(ycoef) <- colnames(y)[qy$pivot][1L:dy]
+z <- svd(qr.qty(qx, qr.qy(qy, diag(1, nr, dy)))[1:dx,], dx, dy)
+xcoef <- backsolve((qx$qr)[1:dx, 1:dx], z$u)
+ycoef <- backsolve((qy$qr)[1:dy, 1:dy], z$v)
 cor <- z$d
+
+ycoef - cancor(pop, oec)$ycoef
+xcoef - cancor(pop, oec)$xcoef
+all.equal(cor, cancor(pop, oec)$cor)
