ca8cce04934b88c82646183696777d26bd98cadd
Francis DiTraglia <fditra@sas.upenn.edu>
2014-03-13 13:58:10 -0400

    extended function pointers example
    Now I have a working example that's very close to what I need to use in
    the FMSC code.

--- ChooseInstruments/function_pointers.cpp
+++ ChooseInstruments/function_pointers.cpp
@@ -1,9 +1,12 @@
 //This file contains a simple example of function pointers in C++
-//It is similar to an example from: 
-//http://www.newty.de/fpt/intro.html
+//I learned about this topic from http://www.newty.de/fpt/intro.html
+//and my first example is similar to one given there.
 
-#include <Rcpp.h>
+// [[Rcpp::depends(RcppArmadillo)]]
+
+#include <RcppArmadillo.h>
 using namespace Rcpp;
+using namespace arma;
 
 
 //These will be passed as arguments to another function
@@ -37,3 +40,50 @@ double bar (double a, double b){
   double answer = ResultPlusOne(a, b, &Minus);
   return answer;
 }
+
+//Now how about a more interesting example. This is closer to what
+//I'll actually use in my FMSC code
+
+//This is the basic code for an "indicator vector"
+colvec indicator (colvec b_valid, int which_index){
+  colvec D_mu = zeros<colvec>(b_valid.n_elem);
+  D_mu(which_index) = 1;
+  return D_mu;
+}
+
+//From which particular indicators can be generated
+colvec first_element (colvec b_valid){
+  return indicator(b_valid, 0);
+}
+colvec second_element (colvec b_valid){
+  return indicator(b_valid, 1);
+}
+
+//Here's an example that actually depends on b_valid
+colvec squares (colvec b_valid){
+  return pow(b_valid, 2);
+}
+
+//This function actually does the computations
+double quadratic_form (colvec b_valid, mat Omega, 
+                       colvec (*pt2Function)(colvec)){
+  colvec D_mu = pt2Function(b_valid);
+  return as_scalar(D_mu.t() * Omega * D_mu);
+}
+
+//And these functions simply pass arguments to quadratic_form
+// [[Rcpp::export]]
+double baz1 (colvec b_valid, mat Omega){
+  return quadratic_form(b_valid, Omega, &first_element);
+}
+// [[Rcpp::export]]
+double baz2 (colvec b_valid, mat Omega){
+    return quadratic_form(b_valid, Omega, &second_element);  
+}
+
+// [[Rcpp::export]]
+double baz3 (colvec b_valid, mat Omega){
+    return quadratic_form(b_valid, Omega, &squares);  
+}
+
+
