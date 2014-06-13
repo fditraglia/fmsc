8648f88bc424cbea5bdc26d90463abd1adfceb69
Francis DiTraglia <fditra@sas.upenn.edu>
2014-06-13 17:10:58 -0400

    removed some blank lines

--- EmpiricalExample/run_empirical_example.R
+++ EmpiricalExample/run_empirical_example.R
@@ -2,7 +2,6 @@
 #Last Updated: June 13th, 2014
 #This script runs the empirical example.
 
-
 #My empirical example is based on Carstensen & Gundlach (2006) which I abbreviate C&G in the comments below. Specifically, I examine the instrument selection exercise from Table 2 of their paper. This table uses lngdpc as the dependent variable and rule and malfal as the independent variables throughout. The panels of the table look at how adding additional instruments changes the results.
 
 #Note that when estimating with the baseline instruments we could use 45 observations (as CG do in their paper), since lnmort and maleco are observed for Vietnam. We choose not to, however, so that everything is fixed across different specifications except the instruments used. The difference is miniscule in any case.
