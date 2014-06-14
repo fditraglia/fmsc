89cf985aa3e157495fb0c2f277feebe2cba594a7
Francis DiTraglia <fditra@sas.upenn.edu>
2014-06-14 19:05:25 -0400

    cleanup

--- EmpiricalExample/table_2SLS_results.R
+++ EmpiricalExample/table_2SLS_results.R
@@ -3,45 +3,56 @@
 
 #This script constructs a table of 2SLS Results for all instrument sets considered in my empirical example. It should not be run on its own: it is called by run_empirical_example.R
 
-range <- 1:2
-foo.raw <- do.call("cbind", tsls.summaries[range])
-foo <- foo.raw
-rNames <- rownames(foo)
-nCols <- ncol(foo)
-foo <- apply(foo, c(1,2), format, nsmall = 2) #format works like as.character but with more precise formatting commands. We need it here to keep the trailing zeros from being deleted
-foo <- apply(foo, c(1,2), function(x) paste0('$', x, '$'))
-foo <- cbind(rNames, foo)
-foo <- apply(foo, 1, function(x) paste(x, collapse = ' & '))
-names(foo) <- NULL
-foo <- paste(foo, collapse = "\\\\ \n")
-cat(foo)
+#Since I need to do this twice (two panels of results so it fits on one page), I've wrapped everything into a function. 
+make.table <- function(range){
 
-bar.raw <- instrument.blocks[range]
-full <- unique(unlist(bar.raw))
-bar <- lapply(bar.raw, function(x) replace(rep(NA, length(full)), match(x, full), x))
-bar <- t(do.call("rbind", bar))
-bar[is.na(bar)] <- ""
-bar <- apply(bar, c(1,2), function(x) paste0('\\multicolumn{2}{c}{', x, "}"))
-bar <- apply(bar, 1, function(x) paste(x, collapse = " & "))
-bar <- paste('&', bar)
-bar <- paste(bar, collapse = "\\\\ \n")
-cat(bar)
+  #2SLS Results Panel
+  results.raw <- do.call("cbind", tsls.summaries[range])
+  results <- results.raw
+  rNames <- rownames(results)
+  nCols <- ncol(results)
+  results <- apply(results, c(1,2), format, nsmall = 2) #format allows us to keep trailing zeros
+  results <- apply(results, c(1,2), function(x) paste0('$', x, '$'))
+  results <- cbind(rNames, results)
+  results <- apply(results, 1, function(x) paste(x, collapse = ' & '))
+  names(results) <- NULL
+  results <- paste(results, collapse = "\\\\ \n")
+  
+  #Instrument Blocks Panel
+  blocks.raw <- instrument.blocks[range]
+  full <- unique(unlist(blocks.raw))
+  blocks <- lapply(blocks.raw, function(x) replace(rep(NA, length(full)), match(x, full), x))
+  blocks <- t(do.call("rbind", blocks))
+  blocks[is.na(blocks)] <- ""
+  blocks <- apply(blocks, c(1,2), function(x) paste0('\\multicolumn{2}{c}{', x, "}"))
+  blocks <- apply(blocks, 1, function(x) paste(x, collapse = " & "))
+  blocks <- paste('&', blocks)
+  blocks <- paste(blocks, collapse = "\\\\ \n")
 
-#Head of Table
-panel.numbers <- paste0('\\multicolumn{2}{c}{', range, '}')
-panel.numbers <- paste(panel.numbers, collapse = ' & ')
-panel.numbers <- paste('&', panel.numbers)
-cNames <- colnames(foo.raw)
-cNames <- paste0('\\multicolumn{1}{c}{\\emph{', cNames, '}}')
-cNames <- paste(cNames, collapse = ' & ')
-cNames <- paste('&', cNames)
-table.head <- paste0('\\hline \\hline \n', panel.numbers, '\\\\ \n', cNames, '\\\\ \n \\hline \n') 
-cat(table.head)
+  #Head of Table
+  panel.numbers <- paste0('\\multicolumn{2}{c}{', range, '}')
+  panel.numbers <- paste(panel.numbers, collapse = ' & ')
+  panel.numbers <- paste('&', panel.numbers)
+  cNames <- colnames(results.raw)
+  cNames <- paste0('\\multicolumn{1}{c}{\\emph{', cNames, '}}')
+  cNames <- paste(cNames, collapse = ' & ')
+  cNames <- paste('&', cNames)
+  table.head <- paste0('\\hline \\hline \n', panel.numbers, '\\\\ \n', cNames, '\\\\ \n \\hline \n') 
 
-out <- paste0(table.head, ' \n', foo, '\\\\ \n', bar, '\\\\ \n \\hline')
-out <- paste0('\\begin{tabular}{l', paste(rep('r', nCols), collapse = ""), '}\n', out)
-out <- paste0(out, '\n\\end{tabular}')
-cat(out)
+  out <- paste0(table.head, ' \n', results, '\\\\ \n', blocks, '\\\\ \n \\hline')
+  out <- paste0('\\begin{tabular}{l', paste(rep('r', nCols), collapse = ""), '}\n', out)
+  out <- paste0(out, '\n\\end{tabular}')
+  return(out)
+}
 
+#Assemble full table from two separate panels
+panel1 <- make.table(1:6)
+panel2 <- make.table(7:12)
+full.table <- paste(panel1, '\n \\vspace{2em}', panel2)
 
-#There is a very slight discrepancy (a difference of 0.01 in a few places) between these results and those in the original version of my paper. This comes from the way I've chosen to handle missing observations: I now exclude Vietnam from the dataset completely, since we only observe the baseline instruments for this country and I want to hold everything constant except the instruments in this exercise. In constrast CG use Vietnam when it is available, as did I in the original version of the empirical example. Again, the difference in results is miniscule.
+#Make "MalfalSq" and "RuleSq" look prettier
+#gsub("MalfalSq", full.table, "\\emph{malfal}$^2$")
+#gsub("RuleSq", full.table, "\\emph{rule}$^2$")
+
+cat(full.table, file = "table_2sls_results.tex")
+rm(full.table, panel1, panel2, make.table)
