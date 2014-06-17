fb175890686c175a1da4f4df208a7478dc047828
Francis DiTraglia <fditra@sas.upenn.edu>
2014-06-17 15:30:37 -0400

    add comments and cleanup line breaks

--- OLSvsIVsim/run_OLSvsIV_sim.R
+++ OLSvsIVsim/run_OLSvsIV_sim.R
@@ -6,3 +6,16 @@ library(tikzDevice)
 sourceCpp("functions.cpp")
 
 set.seed(1928)
+
+#Calculate RMSE results for the simulation
+#and store them in rmse_results.Rdata
+source("rmse_calculations.R")
+
+#Open rmse_results.Rdata
+#plot the results 
+#and export in tikz format
+#source("rmse_plots.R")
+
+#source("CI_calculations.R")
+
+#source("CI_plots.R")
