79a700b9722214e8f7a8b9b593b8852d0a80085f
Francis DiTraglia <fditra@sas.upenn.edu>
2014-06-23 11:32:25 -0400

    add more testing code
    All tests were successful

--- ChooseInstruments/test_fmsc_code.R
+++ ChooseInstruments/test_fmsc_code.R
@@ -30,13 +30,37 @@ y <- test.data$y
 Z1 <- with(test.data, cbind(z0, z1))
 Z2 <- with(test.data, cbind(z2, z3))
 
-tsls_est_cpp(X, y, Z1) #Valid model estimates
-tsls_est_cpp(X, y, cbind(Z1, Z2)) #Full model estimates
-#Additional candidates
-tsls_est_cpp(X, y, cbind(Z1, test.data$z2))
-tsls_est_cpp(X, y, cbind(Z1, test.data$z3))
-
 #Specify additional candidates using indicators for the columns of Z2 to be used in estimation
 add.cand <- cbind(c(1, 0), c(0, 1))
 foo <- fmsc_test(X, y, Z1, Z2, as.matrix(0))
 bar <- fmsc_test(X, y, Z1, Z2, add.cand)
+
+#Check that we're getting the right estimates
+#stored in the correct order
+all.equal(foo$Estimates, bar$Estimates[,c(1,4)])
+
+est1 <- tsls_est_cpp(X, y, Z1) #Valid model estimates
+est2 <- tsls_est_cpp(X, y, cbind(Z1, test.data$z2)) #Add. candidate #1
+est3 <- tsls_est_cpp(X, y, cbind(Z1, test.data$z3)) #Add. candidate #2
+est4 <- tsls_est_cpp(X, y, cbind(Z1, Z2)) #Full model estimates
+est <- cbind(est1, est2, est3, est4)
+
+all.equal(est, bar$Estimates)
+
+#tau, Psi, tau.outer and Bias.mat
+#should be the same regardless of
+#whether we include add.cand
+all.equal(foo$tau, bar$tau)
+all.equal(foo$Psi, bar$Psi)
+all.equal(foo$tau.outer, bar$tau.outer)
+all.equal(foo$Bias.mat, bar$Bias.mat)
+
+#Valid and Full model avar.inner
+#and sq.bias.inner should be the same
+#regardless of whether we include
+#add.cand
+all.equal(foo$avar.inner, bar$avar.inner[,,c(1,4)])
+all.equal(foo$sq.bias.inner, bar$sq.bias.inner[,,c(1,4)])
+
+#Now that I knows the results are consistent between foo and bar, I need to check that the calculations are in fact correct. I'll do this by directly implementing the FMSC calculations inline
+
