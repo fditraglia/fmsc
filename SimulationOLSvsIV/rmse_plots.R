33c34f5342b03a14b0f38b2834dd32343282ed6c
Francis DiTraglia <fditra@sas.upenn.edu>
2014-06-29 18:57:36 -0400

    update RMSE plotting code
    Because the output of the script that calculates MSE for the OLS vs IV
    simulation was changed in the preceding commit to be a data frame
    rather than a matrix, I had to update this script as well.
    
    The basic structure is the same. Essentially, I "repackage" the results
    of the MSE calculations for compatibility with the existing plotting
    code.
    
    There also a few minor tweaks here and there but the only important one
    is that the user must now specify the layout of the panels in the plot
    by providing the number of rows and columns. I've added this as a
    "macro" at the top of the file to make it easy to change.

--- SimulationOLSvsIV/rmse_plots.R
+++ SimulationOLSvsIV/rmse_plots.R
@@ -3,22 +3,32 @@ coarse.pi <- results$coarse.pi
 coarse.rho <- results$coarse.rho
 rm(results)
 
+#Set parameters defining plot layout, etc.
+nRows <- 3
+nCols <- 3
 plot.width <- 6
 plot.height <- 7
 legend.inset <- 0.03
-
 line.types <- 1:10
 line.colors <- c("black", "red", "blue", "orange", "green", "cyan")
 line.width <- 2
+par(mar = c(3,3,2,1), mgp = c(2, 0.7, 0), tck = -0.01) 
 
-par (mar = c(3,3,2,1), mgp = c(2, 0.7, 0), tck = -0.01) 
-
-
+#A "panel" is a subset of the simulation results in which
+#all but one parameter is fixed. Given a panel and a list
+#of columns to plot, this function detects which of the
+#parameters p and r is being held constant and sets up the
+#plot appropriately. The default relative = TRUE gives RMSE
+#relative to the oracle, i.e. the infeasible estimator that
+#takes the lower RMSE envelope of OLS and TSLS.
 rmse.plot <- function(panel, col.list, relative = TRUE, 
                       legend.pos = "topright"){
-  panel[,-c(1:3)] <- apply(panel[,-c(1:3)], c(1,2), sqrt)
-  panel <- as.data.frame(panel) #allows $ column selection
+  
+  results.cols <- setdiff(names(panel), c("n", "p", "r"))
+  panel[,results.cols] <- sqrt(panel[,results.cols])
+  panel[,setdiff(names(panel), c("n", "p", "r"))]
   y <- panel[,col.list] 
+  
   if(relative){
     oracle <- pmin(panel$OLS, panel$TSLS)
     y <- y[,!(col.list %in% c("OLS", "TSLS")), drop = FALSE]
@@ -27,8 +37,10 @@ rmse.plot <- function(panel, col.list, relative = TRUE,
   }else{
     y.label <- "RMSE"
   }
+  
   p <- unique(panel$p)
   r <- unique(panel$r)
+  
   if(length(p) != 1){
     x <- p
     x.label <- "$\\pi$"
@@ -40,6 +52,7 @@ rmse.plot <- function(panel, col.list, relative = TRUE,
     fixed.value <- p
     fixed.label <- "\\pi"
   }
+  
   n <- unique(panel$n)
   plot.title <- paste0("$N=", n, ", \\;", 
                        fixed.label, "=", fixed.value, "$")
@@ -55,59 +68,80 @@ rmse.plot <- function(panel, col.list, relative = TRUE,
 
 
 
-plot.grid <- function(results, ...){
-  nCols <- length(results)
-  nRows <- lapply(results, length)[[1]]
-  par(mfcol = c(nRows, nCols))
-  for(i in 1:nCols){
-    for(j in 1:nRows){
-      rmse.plot(results[[i]][[j]], ...)
-    }
-  }
-  par(mfcol = c(1,1))
+#Convert simulation results into lists of "panels" for
+#passing to rmse.plot
+coarse.pi.params <- expand.grid(n = unique(coarse.pi$n), 
+                                p = unique(coarse.pi$p))
+coarse.pi <- mapply(function(x, y) 
+                    subset(coarse.pi, n == x & p == y),
+                    x = coarse.pi.params$n,
+                    y = coarse.pi.params$p, 
+                    SIMPLIFY = FALSE)
+
+coarse.rho.params <- expand.grid(n = unique(coarse.rho$n), 
+                                 r = unique(coarse.rho$r))
+coarse.rho <- mapply(function(x, y) 
+                     subset(coarse.rho, n == x & r == y),
+                     x = coarse.rho.params$n,
+                     y = coarse.rho.params$r, 
+                     SIMPLIFY = FALSE)
+
+
+#This function takes a list of panels and repeatedly calls
+#rmse.plot to construct a grid of plots. We can pass it 
+#any additional arguments we like and they will "fall through"
+#to rmse.plot
+plot.grid <- function(results, nRows, nCols, ...){
+  par(mfrow = c(nRows, nCols))
+  lapply(results, rmse.plot, ...)
+  par(mfrow = c(1,1))
 }
 
+
+#Columns to include in the different plots
 baseline <- c("OLS", "TSLS", "FMSC")
 relative.DHW <- c("OLS", "TSLS", "FMSC", "DHW90", "DHW95")
 relative.all <- c("OLS", "TSLS", "FMSC", "AVG","DHW90", "DHW95")
 
 
+#Create plots and export as tikz files
 tikz('./Results/RMSE_coarse_pi_baseline.tex',
      width = plot.width, height = plot.height)
-  plot.grid(coarse.pi, baseline, relative = FALSE,
+  plot.grid(coarse.pi, nRows, nCols, baseline, relative = FALSE,
             legend.pos = "topleft")
 dev.off()
 
 tikz('./Results/RMSE_coarse_pi_relative_DHW.tex',
      width = plot.width, height = plot.height)
-plot.grid(coarse.pi, relative.DHW)
+plot.grid(coarse.pi, nRows, nCols, relative.DHW)
 dev.off()
 
 tikz('./Results/RMSE_coarse_pi_relative_all.tex',
      width = plot.width, height = plot.height)
-plot.grid(coarse.pi, relative.all)
+plot.grid(coarse.pi, nRows, nCols, relative.all)
 dev.off()
 
 tikz('./Results/RMSE_coarse_rho_baseline.tex',
      width = plot.width, height = plot.height)
-plot.grid(coarse.rho, baseline, relative = FALSE)
+plot.grid(coarse.rho, nRows, nCols, baseline, relative = FALSE)
 dev.off()
 
 tikz('./Results/RMSE_coarse_rho_relative_DHW.tex',
      width = plot.width, height = plot.height)
-plot.grid(coarse.rho, relative.DHW, 
+plot.grid(coarse.rho, nRows, nCols, relative.DHW, 
           legend.pos = "topleft")
 dev.off()
 
 tikz('./Results/RMSE_coarse_rho_relative_all.tex',
      width = plot.width, height = plot.height)
-plot.grid(coarse.rho, relative.all)
+plot.grid(coarse.rho, nRows, nCols, relative.all)
 dev.off()
 
 
 #Clean up
-rm(coarse.pi, coarse.rho)
+rm(coarse.pi, coarse.rho, nRows, nCols)
 rm(line.colors, line.types, line.width)
 rm(rmse.plot, plot.grid)
 rm(baseline, relative.DHW, relative.all)
 rm(plot.width, plot.height, legend.inset)
+rm(coarse.pi.params, coarse.rho.params)
