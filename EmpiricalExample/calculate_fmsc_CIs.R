c5ea839b770cc39396678cec4f05e3d10edba897
Francis DiTraglia <fditra@sas.upenn.edu>
2014-07-05 17:31:01 -0400

    lots of progress on CIs for empirical example

--- EmpiricalExample/calculate_fmsc_CIs.R
+++ EmpiricalExample/calculate_fmsc_CIs.R
@@ -26,115 +26,151 @@ R.tau <- function(x, delta = 0.05){
   t(diff) %*% tau.var.inv %*% diff < crit
 }
 
+# To construct valid confidence intervals, we first need code
+# to evaluate FMSC_S(tau.star, M) for each specification S
+# at simulation draw M and specified value tau.star for the 
+# bias parameter. 
+#
+# We calculate FMSC from an estimate of the ABIAS and AVAR of
+# of a given estimator of theta. The AVAR estimate for each
+# specification is:
+#
+#     AVAR = K.S * Xi.S * Omega * Xi.S' * K.S
+#
+# This doesn't depend on tau.star or M and we've already 
+# computed it
+avar.S <- fmsc.ingredients$avar
 
-# The first step is contructing a function that can evaluate
-# FMSC_S(tau.star, M). The "meat" of the sandwich doesn't
-# depend on the target parameter so we'll start here. There 
-# are two components, one of which depends on M and tau.star
-# and one of which doesn't. 
 
-# The AVAR component for each candidate is *fixed* and we've 
-# already computed it: these are our estimates of the product
-# K.S * XI.S * Omega * Xi.S' * K.S'
-avar <- fmsc.ingredients$avar
-
-# The Bias component *does* depend on M and tau.star:
-#     K.S * Xi.S * BIAS.MAT * Xi.S' * K.S
-# where BIAS.MAT is a 2x2 block matrix whose only
-# non-zero block is the lower diagonal block, which is:
+# The ABIAS.sq estimate depends on the matrix
+#                 [ 0  0 ]
+#     BIAS_MAT =  [ 0  B ]
+#
+# where
+#
 #     B = B1 - B2
-# where:
-#     B1 = (Psi * M + tau.star)(Psi * M + tau.star)'
-#     B2 = Psi * Omega * Psi'
-# Notice that only B1 depends on M and tau.star. Moreover,
-# B2 is simply the variance matrix of tau.hat which we
-# already computed above
+#     B1 = (Psi * M + tau.star) * (Psi * M + tau.star)'
+#     B2 =  Psi * Omega * Psi
+#
+# Note that *only* B1 depends on tau.star or M. Moreover,
+# we have already computed B2: it is simply the variance 
+# matrix of tau.hat
 B2 <- tau.var
 
-# Now, we do not in fact need to form BIAS.MAT. It is
-# a waste of storage and flops to explicitly multiply
-# all of the zeros with the zeros in Xi.S. The only 
-# part of K.S that plays a role in the ABIAS is the 
-# part that multiplies the *non-zero* elements of the
-# matrix BIAS.MAT. So what we really want is to subset
-# K.S. Remember that K.S has as many rows as there are
-# regressors, and as many columns as there are instruments
-# in specification S. Since we always include the valid
-# instruments, we can partition K.S as follows:
+# Now, BIAS_MAT enters the ABIAS formula as follows:
 #
-#   K.S = [K.S(valid)  K.S(suspect)]
+#   ABIAS.sq = K.S * Xi.S * BIAS_MAT * Xi.S' * K.S
 #
-# where K.S(valid) corresponds to the valid instruments 
-# and K.S(suspect) corresponds to the potentially invalid
-# instruments included in specification S. Accordingly, 
-# we'll first extract K.S(suspect) for each S:
+# Although written as a matrix multiplication, the 
+# combination of the Xi.S matrices with the zero 
+# elements of BIAS_MAT is really just a pair of 
+# subsetting operations that serve to remove the
+# the elements of K.S that correspond to the valid
+# instruments and remove the elements of B that 
+# correspond to instruments from z2 that are not
+# used in specification S. Rather than explicitly
+# forming BIAS_MAT, we'll just take the appropiate
+# subsets.
+#
+# First, we'll get the bias contribution that comes
+# from B2 since this doesn't depend on tau or M.
+#
+# In this particular application there are three
+# instruments in z1: a constant, maleco and lnmort.
+# This means that the *first three* columns of K.S
+# are "zeroed out" by BIAS_MAT for each S. The 
+# remaining columns correspond to the "suspect
+# instruments:
 K <- fmsc.ingredients$K 
 K.suspect <- lapply(K, function(mat) mat[,-c(1:3)])
 
-# All that pre-multiplying by Xi.S and post-multiplying
-# by Xi.S' does is subset the non-zero block of BIAS.MAT
-# to correspond to the instruments used in specification
-# S. We can do this without explicitly forming the Xi
-# matrices by using the candidate indicators that we 
-# used in the FMSC calculation. These correspond to the
-# columns of z2 used in estimation. We'll extract them
-# and convert them from a matrix ofzeros and ones to a
-# list of TRUE and FALSE. 
+# To subset B, well use logical indicators constructed
+# as follows:
 cand <- fmsc.ingredients$cand
 cand <- lapply(seq_len(ncol(cand)), 
                function(i) as.logical(cand[,i]))
 
-# Now we can use cand to do the subsetting that Xi
-# accomplishes via matrix multiplication. First, 
-# we'll do this for B2, turning it into a list where
-# each element refers to a particular candidate
-B2 <- lapply(cand, function(x) B2[x,x])
-
-# And now pre-multiply by the *relevent* part of K
-# and post-multiple by the *relevant* part of K.S
-# the result will be a list of matrices each of which
-# has the same dimensions
-B2 <- lapply(seq_along(K.suspect), function(i)
-              K.suspect[[i]] %*% B2[[i]] %*% t(K.suspect[[i]]))
-
-# As a sanity check, let's make sure we can get the same
-# squared asymptotic bias matrix as the C++ code gave us
-# First we need to extract it and convert it to a list
-sqbias.cpp <- fmsc.ingredients$sqbias
-sqbias.cpp <- lapply(1:(dim(sqbias.cpp)[3]),
-                     function(i) sqbias.cpp[,,i])
-tt <- lapply(cand, function(x) tau.hat[x] %o% tau.hat[x])
-K.tt.K <- lapply(seq_along(tt), function(i)
-                K.suspect[[i]] %*% tt[[i]] %*% t(K.suspect[[i]]))
-sqbias.R <- lapply(seq_along(B2), function(i)
-                K.tt.K[[i]] - B2[[i]])
-all.equal(sqbias.R, sqbias.cpp)
-rm(sqbias.cpp, tt, K.tt.K, sqbias.R)
-
-# Now that we know this works, we turn our attention to B1.
-# First, expand as follows:
-#     B1 = A1 + A2' + A2 + A3
-# where
-#     A1 = Psi * M * M' * Psi'
-#     A2 = Psi * M * tau.star'
-#     A3 = tau.star * tau.star'
+# Now we can calculate the contribution to the squared
+# asymptotic bias that comes from B2:
+#         
+#                        [ 0  0  ]
+#     B2.S = K.S * Xi.S  [ 0  B2 ] * Xi.S' * K.S
+#
+# If we define B1.S analogously, then:
+#
+#     ABIAS.sq(S) = B1.S - B2.S
 # 
+# That is, B2.S enters *negatively*
+S.contrib <- function(i, Inner){
+  S <- cand[[i]]
+  K.S <- K.suspect[[i]]
+  K.S %*% Inner[S,S] %*% t(K.S)
+}
+B2.S <- sapply(seq_along(cand), S.contrib, Inner = B2,
+               simplify = "array")
 
+# We can check that this works by re-computing the squared
+# asymptotic bias matrices that we already have from the 
+# C++ code used to calculate the FMSC in the empirical 
+# example
+sqbias.cpp <- fmsc.ingredients$sqbias
+foo <- sapply(seq_along(cand), S.contrib, 
+              Inner = tau.hat %o% tau.hat, simplify = "array")
+sqbias.R <- foo - B2.S
+all.equal(sqbias.R, sqbias.cpp)  
+rm(foo, sqbias.cpp, sqbias.R)
+
+# Now, as it happens there we can exploit a substantial 
+# simplification in this example because the target
+# parameters we consider are simply *elements* of the
+# underlying parameter vector beta. This means that we 
+# only need to store elements on the *main diagonal* of
+# B2.S and avar.S
+avar.S <- apply(avar.S, 3, diag)
+B2.S <- apply(B2.S, 3, diag)
+
+# While doesn't save us particularly many computations for
+# avar.S and B2.S it's *incredibly* helpful for dealing with 
+# B1.S(M, tau.star)
+#
+#                        [ 0  0  ]
+#     B1.S = K.S * Xi.S  [ 0  B1 ] * Xi.S' * K.S
+#
+#     B1 = (Psi * M + tau.star) * (Psi * M + tau.star)'
+#
+# since it means we *never* have to explicitly form and
+# store the outer product matrices. First, we pre-compute
+# Psi * M
 Psi.M <- Psi %*% M
-Psi.M <- lapply(1:ncol(Psi.M), function(i)
-                Psi.M[,i])
-Psi.M <- Psi.M[[1]]
-tau.star <- tau.hat
+
+
+B1.diag <- lapply(seq_along(cand), function(i)
+          (K.suspect[[i]] %*% (Psi.M + tau.star)[cand[[i]],])^2)
+                    
+sqbias.diag.sim <- lapply(seq_along(B1.diag), function(i)
+                           B1.diag[[i]] - B2.diag[,i])
+
+FMSC.diag.sim <- sapply(seq_along(sqbias.diag.sim), function(i)
+                        sqbias.diag.sim[[i]] + avar.diag[,i],
+                        simplify = "array")
+
+posFMSC.diag.sim <- sapply(seq_along(sqbias.diag.sim), function(i)
+                pmax(sqbias.diag.sim[[i]], 0) + avar.diag[,i],
+                simplify = "array")
+
+
+apply(FMSC.diag.sim[3,,], 1, which.min)
+bar <- apply(posFMSC.diag.sim[3,,], 1, which.min)
+sapply(bar, function(i) K[[bar[i]]][3,] %*% M[cand[[bar[i]]],i])
+
+
+
+#Sanity Check
+testy <- apply(K.tt.K, 3, diag) - B2.diag + avar.diag
+all.equal(testy[2,], fmsc.values$rule[,1])
+all.equal(testy[3,], fmsc.values$malfal[,1])
+
+
 n <- nrow(CGdata)
-
-A1 <- outer(Psi.M, Psi.M)
-A2 <- Psi.M %*% t(tau.star)
-A3 <- outer(tau.star, tau.star)
-B1 <- A1 + t(A2) + A2 + A3
-B1 <- lapply(cand, function(x) B1[x,x])
-B1 <- lapply(seq_along(K.suspect), function(i)
-            K.suspect[[i]] %*% B1[[i]] %*% t(K.suspect[[i]]))
-
-lapply(Psi.M, function(x) x %o% x)
 
 
