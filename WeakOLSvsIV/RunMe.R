dfe195f84d84b45b9036fdded9a1662dd99306fd
Francis DiTraglia <fditra@sas.upenn.edu>
2016-07-18 14:42:29 -0400

    remove scripts for CIs and references to then in RunMe.R since this simulation is only for risk properties

--- WeakOLSvsIV/RunMe.R
+++ WeakOLSvsIV/RunMe.R
@@ -25,18 +25,3 @@ source("mse_calculations.R")
 #plot the results 
 #and export in tikz format to ./Results/
 source("rmse_plots.R")
-
-#Calculate Confidence Interval results for simulation
-#and store them in ./Results/CI_results.Rdata
-source("CI_calculations.R")
-
-#Load CI_results.Rdata and 
-#format into two lists of tables
-#One for coverage probability
-#and another for median CI width relative
-#to TSLS
-source("CI_Rtables.R")
-
-#Convert the lists of CI tables
-#to LaTeX and output to ./Results/
-source("CI_TeXtables.R")
